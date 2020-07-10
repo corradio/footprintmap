29c620da9e0ba17c0ef6a8cb3e7243b38f50c7de
Olivier Corradi <olivier.corradi@gmail.com>
2020-07-10 23:08:19 +0200

    Clean up

--- web/src/layout/leftpanel/infotext.js
+++ web/src/layout/leftpanel/infotext.js
@@ -26,29 +26,19 @@ export default () => (
         {__('panel-initial-text.datasources')}
       </a>
       ).{' '}
-      <span
-        dangerouslySetInnerHTML={{
-          __html: __(
-            'panel-initial-text.contribute',
-            'https://github.com/tmrowco/electricitymap-contrib#adding-a-new-region',
-          ),
-        }}
-      />
-      .
     </p>
     <p>
       {__('footer.foundbugs')} <a href="https://github.com/tmrowco/electricitymap-contrib/issues/new" target="_blank">{__('footer.here')}</a>.<br />
     </p>
-    <p>
-      {__('footer.likethisvisu')} <a href="https://docs.google.com/forms/d/e/1FAIpQLSc-_sRr3mmhe0bifigGxfAzgh97-pJFcwpwWZGLFc6vvu8laA/viewform?c=0&w=1" target="_blank">{__('footer.loveyourfeedback')}</a>!
-    </p>
-    <p>
-      {__('footer.faq-text')}
-      {' '}
-      <Link to={{ pathname: '/faq', search: useLocation().search }}>
-        <span className="faq-link">{__('footer.faq')}</span>
-      </Link>
-    </p>
+    { null && (
+      <p>
+        {__('footer.faq-text')}
+        {' '}
+        <Link to={{ pathname: '/faq', search: useLocation().search }}>
+          <span className="faq-link">{__('footer.faq')}</span>
+        </Link>
+      </p>
+    )}
     <div className="social-buttons">
       <div>
         { /* Facebook share */}
